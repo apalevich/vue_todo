c4249d06feb72b2edad8c3dba64461577667a74a
Artyom Palevich <hello@apalevich.ru>
2022-08-11 12:20:11 +0300

    Added checkbox and delete button

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,18 +1,30 @@
 <template>
-    <div>
-        <p>{{todo.title}}</p>
+    <div
+        class="todo-item"
+        v-bind:class="{'is-complete': todo.completed}"
+    >
+        <input type="checkbox" v-on:click="markComplete">
+        <p class="description">{{todo.title}}</p>
+        <button class="del">x</button>
     </div>
 </template>
 
 <script>
 export default {
     name: 'TodoItem',
-    props: ['todo']
+    props: ['todo'],
+    methods: {
+        markComplete() {
+            // eslint-disable-next-line vue/no-mutating-props
+            this.todo.completed = !this.todo.completed
+        }
+    }
 }
 </script>
 
 <style scoped>
     .todo-item {
+        display: flex;
         background: #f4f4f4;
         padding: 10px;
         border-bottom: 1px #ccc dotted;
@@ -22,11 +34,20 @@ export default {
         text-decoration: line-through;
     }
 
+    input[type="checkbox"] {
+        margin: 0px 8px;
+    }
+
+    .description {
+        flex-grow: 1;
+        line-height: 1.7;
+    }
+
     .del {
         background: #ff0000;
         color: #fff;
         border: none;
-        padding: 5px 9px;
+        padding: 5px 10px;
         border-radius: 50%;
         cursor: pointer;
         float: right;
